dec80b20b8ab9969513292ec57c4a5b1968261e5
dupyao_d <dupyao_d@f112.ub.lan>
2026-07-08 09:27:09 +0800

    feat: update auth-portal.ts fix login

--- src/app/auth-portal/auth-portal.ts
+++ src/app/auth-portal/auth-portal.ts
@@ -1,13 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { InventoryService } from '../inventory.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth-portal',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth-portal.html',
   styleUrl: './auth-portal.css'
 })
@@ -15,50 +14,22 @@ export class AuthPortal {
   private inventoryService = inject(InventoryService);
   private router = inject(Router);
 
-  isRegister: boolean = false;
-  feedbackMessage: string = '';
-
-  authForm = new FormGroup({
+  loginForm = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.minLength(4)]),
     password: new FormControl('', [Validators.required, Validators.minLength(4)])
   });
 
-  toggleForm() {
-    this.isRegister = !this.isRegister;
-    this.feedbackMessage = '';
-    this.authForm.reset();
-  }
+  onLogin() {
+    if (this.loginForm.invalid) return;
 
-  onSubmit() {
-    if (this.authForm.invalid) return;
+    const { username, password } = this.loginForm.value;
 
-    if (this.isRegister) {
-      this.inventoryService.register(this.authForm.value).subscribe({
-        next: () => {
-          this.feedbackMessage = 'Registration successful! You can now log in.';
-          this.isRegister = false;
-          this.authForm.reset();
-        },
-        error: (err) => {
-          this.feedbackMessage = err.error?.error || 'Registration failure.';
-        }
-      });
+    if (username === 'admin' && password === 'APPDEV1') {
+      this.inventoryService.currentUser = { username: 'admin' };
+      
+      this.router.navigate(['/dashboard']);
     } else {
-      if (this.authForm.value.username === 'admin' && this.authForm.value.password === 'APPDEV1') {
-        this.inventoryService.currentUser = 'Professor';
-        this.router.navigate(['/dashboard']);
-        return;
-      }
-
-      this.inventoryService.login(this.authForm.value).subscribe({
-        next: (res: any) => {
-          this.inventoryService.currentUser = res.username;
-          this.router.navigate(['/dashboard']);
-        },
-        error: (err) => {
-          this.feedbackMessage = err.error?.error || 'Authentication denied.';
-        }
-      });
+      alert('Invalid credentials!');
     }
   }
 }
